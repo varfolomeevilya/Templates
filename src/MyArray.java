74c549fd997b2554cd5458f2dadd89f670c45e8a
илья <126683197+varfolomeevilya@users.noreply.github.com>
2023-10-04 13:15:20 +0300

    Update MyArray.java

--- src/MyArray.java
+++ src/MyArray.java
@@ -77,4 +77,93 @@ public class MyArray<E> {
                 '}';
     }
 }
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class MyMatrix<T> {
+    T[] matrix;
+    private int size;
+    public MyMatrix(T[] matrix){
+        this.matrix= matrix;
+
+
+    }
+    public MyMatrix(int size){
+        matrix=(T[]) new Object[size];
+    }
+
+    public void fillArrays(iRandom<T> iRandom){
+        for(int i=0; i<size; i++){
+            matrix[i]= iRandom.random();
+        }
+
+    }
+    public void plus(  Operator<T> operator){
+        int  res = (int)this.matrix[0];
+        for(int i =0; i<this.matrix.length; i++){
+            for(int j=0; j<this.matrix.length; j++){
+                res= this.matrix.length + this.matrix.length;
+            }
+        }
+    }
+    public void minus(){
+        int res=0;
+        for (int i=0; i<this.matrix.length; i++){
+            for(int j=0; j<this.matrix.length; j++){
+                res = this.matrix.length + this.matrix.length;
+            }
+        }
+
+    }
+    public void multip(){
+        int res=0;
+        for(int i=0; i<this.matrix.length; i++){
+            for(int j=0; j<this.matrix.length; j++){
+                res= this.matrix.length * this.matrix.length;
+            }
+        }
+
+    }
+    public void division(){
+        int res=0;
+        for(int i=0; i<this.matrix.length; i++){
+            for(int j=0; j<this.matrix.length; j++){
+                res=this.matrix.length/this.matrix.length;
+            }
+        }
+    }
+    public T maxItem(Comparator<T> comparator){
+        T max=this.matrix[0];
+        for(int i=0; i<size; i++){
+            if(comparator.compare(max, this.matrix[i])>0){
+                max=this.matrix[i];
+            }
+        }
+        return max;
+    }
+    public T searchMinItem(Comparator<T> comparator){
+        T min = this.matrix[0];
+        for(int i=0; i<size; i++){
+            if(comparator.compare(min, this.matrix[i])<0){
+                min=this.matrix[i];
+            }
+        }
+        return min;
+    }
+    public int searchMedium(iMedium<T> imedium) {
+        int sum = 0;
+        for(int i=0; i<this.matrix.length; i++){
+            sum+= imedium.medium(this.matrix[i]);
+        }
+        return sum/this.matrix.length;
+
+    }
+
+    @Override
+    public String toString() {
+        return "MyMatrix{"+ Arrays.toString(matrix)+
+                "}";
+
+    }
+}
 
