c37ecf410bada409b8a292af8504d1284f369ce1
илья <126683197+varfolomeevilya@users.noreply.github.com>
2023-10-04 18:32:19 +0300

    Update HandlerArray.java

--- src/HandlerArray.java
+++ src/HandlerArray.java
@@ -1,15 +1,4 @@
 public interface HandlerArray<E>{
     E randomObject();
 }
-public interface iRandom<T> {
-    T random();
-}
-public interface Medium<E> {
-    int medium(E e);
-}
-public interface iMedium<T> {
-    int medium(T t);
-}
-public interface Operator<T> {
-    T operator();
-}
+
